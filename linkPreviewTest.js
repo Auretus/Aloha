56f688a309c6671e330adf03069513fd21d4cb23
Will Halstead <will.r.halstead@gmail.com>
2020-04-11 09:42:35 -0400

    saving what i got

--- linkPreviewTest.js
+++ linkPreviewTest.js
@@ -1,7 +1,19 @@
-import {getLinkPreview} from 'link-preview-js';
+const URLPreview = require("link-preview-js");
+// const handlebars = require("handlebars");
+var exYouTube;
+var exParsedText;
 
-getLinkPreview('https://www.youtube.com/watch?v=MejbOFk7H6c')
-  .then((data) => console.debug(data));
- 
-getLinkPreview('This is a text supposed to be parsed and the first link displayed https://www.youtube.com/watch?v=MejbOFk7H6c')
-  .then((data) => console.debug(data));
+URLPreview.getLinkPreview("https://www.youtube.com/watch?v=MejbOFk7H6c").then(
+  data => (exYouTube = data)
+);
+
+URLPreview.getLinkPreview(
+  "This is a text supposed to be parsed and the first link displayed https://www.youtube.com/watch?v=MejbOFk7H6c"
+).then(data => (exParsedText = data));
+
+if (exYouTube) {
+  console.log(exYouTube);
+}
+if (exParsedText) {
+  console.log(exParsedText);
+}
