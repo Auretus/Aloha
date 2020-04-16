d1173fffab3cf0a534d939b05ab4b2b84593b219
Alex Joel <alexjoeldelgado@gmail.com>
2020-04-15 20:35:20 -0400

    Heroku setup 2

--- config/config.js
+++ config/config.js
@@ -10,7 +10,7 @@ module.exports = {
     operatorsAliases: false
   },
   test: {
-    username: process.env.DB_USER,
+    username: process.env.DB_USERNAME,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_DATABASE,
     host: process.env.DB_HOST,
