eba5f73368733063c60740cfa76b6b55eb01fd72
Alex Joel <alexjoeldelgado@gmail.com>
2020-04-18 04:48:05 -0400

    Pexels is done

--- public/js/pexelAPI.js
+++ public/js/pexelAPI.js
@@ -2,31 +2,29 @@ $(document).ready(function() {
   $("#changeBg").on("click", () => {
     UIkit.modal($("#bgmodal")).show();
   });
-
+  $.get("/api/pexels/store", function(data) {
+    console.log(data);
+    $("#chat").css(data);
+  });
   function getPictures() {
     var theme = $("#themeInput").val();
-    var settings = {
-      url: `https://api.pexels.com/v1/search?query=${theme}&per_page=15&page=1`,
-      method: "GET",
-      timeout: 0,
-      headers: {
-        Authorization:
-          "563492ad6f917000010000015a007adfef72497f8528d58257792ed9"
-      }
-    };
-    $.ajax(settings).then(function(res) {
-      for (i = 0; i < res.photos.length; i++) {
+    $.post("/api/pexels", { query: theme }).then(function(data) {
+      var photos = data.photos;
+      for (i = 0; i < photos.length; i++) {
         var picThumb = $(
-          `<img class="bgPic" src="${res.photos[i].src.large}" height="100px" width="100">`
+          `<img class="bgPic" src="${photos[i].src.large}" height="100px" width="100">`
         );
         $("#bgOptions").append(picThumb);
       }
       $(".bgPic").on("click", e => {
         var imageSource = $(e.target).attr("src");
         $("#chat").css("background-image", `url("${imageSource}")`);
+        $.post("/api/pexels/store", {
+          "background-image": `url("${imageSource}")`
+        });
       });
+      $("#themeInput").val("");
     });
-    $("#themeInput").val("");
   }
   $("#searchIcon").on("click", getPictures);
 });
---
+++ routes/pexel-api-routes.js
@@ -0,0 +1,44 @@
+require("dotenv").config();
+const PexelsAPI = require("pexels-api-wrapper");
+var pexelsClient = new PexelsAPI(process.env.PEXELS_API_KEY);
+var store = require("store");
+
+module.exports = function(app) {
+  app.get("/api/pexels", function(req, res) {
+    res.send();
+  });
+  app.post("/api/pexels", function(req, res) {
+    var theme = req.body.query;
+    pexelsClient
+      .search(theme, 15, 1)
+      .then(function(result) {
+        res.send(result);
+      })
+      .catch(function(e) {
+        console.err(e);
+      });
+  });
+  app.get("/api/pexels/store", function(req, res) {
+    // console.log(req.user);
+    if (!store.get(req.user.id) || store.get(req.user.id).name != req.user.id) {
+      res.send({});
+    } else {
+      store.each(function(value, key) {
+        console.log(key, "==", value);
+      });
+      var chatBG = store.get(req.user.id);
+      console.log(chatBG);
+      res.send(chatBG.chatBG);
+    }
+  });
+  app.post("/api/pexels/store", function(req, res) {
+    // var sessionID = req.socket.parser.incoming.sessionID;
+    var chatObject = {
+      name: req.user.id,
+      chatBG: req.body
+    };
+    console.log(chatObject);
+    store.set(req.user.id, chatObject);
+    res.end();
+  });
+};
