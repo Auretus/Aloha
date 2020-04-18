236f62794c351d90a33dbf909a2b3faa4681555b
Will Halstead <will.r.halstead@gmail.com>
2020-04-17 22:45:00 -0400

    oops

--- models/user.js
+++ models/user.js
@@ -2,39 +2,41 @@
 var bcrypt = require("bcryptjs");
 // Creating our User model
 module.exports = function(sequelize, DataTypes) {
-  var User = sequelize.define("User", {
-    // The email cannot be null, and must be a proper email before creation
-    email: {
-      type: DataTypes.STRING,
-      allowNull: false,
-      unique: true,
-      validate: {
-        isEmail: true
+  var User = sequelize.define(
+    "User",
+    {
+      // The email cannot be null, and must be a proper email before creation
+      email: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        unique: true,
+        validate: {
+          isEmail: true
+        }
+      },
+      // The password cannot be null
+      password: {
+        type: DataTypes.STRING,
+        allowNull: false
+      },
+      username: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        unique: true
+      },
+      userHash: {
+        type: DataTypes.STRING,
+        allowNull: true
       }
     },
-    // The password cannot be null
-    password: {
-      type: DataTypes.STRING,
-      allowNull: false
-    },
-    username: {
-      type: DataTypes.STRING,
-      allowNull: false,
-      unique: true
-    },
-    userHash: {
-      type: DataTypes.STRING,
-      allowNull: true
-    }
-  });
-  User.associate = function(models) {
-    User.belongsToMany(models.Conversation, {
-      as: [participant],
-      through: [userConversation],
-      foreignKey: "userRowId",
-      onDelete: "cascade"
-    });
-  };
+    { timestamps: false }
+  );
+  // User.associate = function(models) {
+  //   User.belongsToMany(models.Conversation, {
+  //     through: "UserConversation",
+  //     onDelete: "cascade"
+  //   });
+  // };
   // Creating a custom method for our User model. This will check if an unhashed password entered by the user can be compared to the hashed password stored in our database
   User.prototype.validPassword = function(password) {
     return bcrypt.compareSync(password, this.password);
--- models/userConversation.js
+++ models/userConversation.js
@@ -1,8 +1,18 @@
-module.exports = function(sequelize, DataTypes) {
-  const UserConversation = sequelize.define("UserConversation", {
-    name: {
-      type: DataTypes.STRING,
-      primaryKey: true
-    }
-  });
+module.exports = function(sequelize) {
+  const UserConversation = sequelize.define(
+    "UserConversation",
+    {},
+    { timestamps: false }
+  );
+  UserConversation.associate = models => {
+    models.User.belongsToMany(models.Conversation, {
+      through: "UserConversation",
+      onDelete: "cascade"
+    });
+    models.Conversation.belongsToMany(models.User, {
+      through: "UserConversation",
+      foreignKey: "conversationRowId"
+    });
+  };
+  return UserConversation;
 };
